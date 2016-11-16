aa1a1f6e8bd9b176ce135149baf96d990d449aa5
Jeromy Hettinga <jeromy.hettinga97@casema.nl>
2016-11-16 10:19:09 +0100

    Created roling Goomba

---
+++ Assets/Scripts/GoombaController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GoombaController : MonoBehaviour {
+
+    public int speed;
+
+    private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        rb.velocity = new Vector3(speed, 0.0f, 0.0f);
+	}
+}
