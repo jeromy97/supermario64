59c775a47f9e0a59d9dcc16b5c100397ac077ea3
Jeromy Hettinga <jeromy.hettinga97@casema.nl>
2016-11-18 11:54:25 +0100

    Goomba is walking around and moves toward the player when he gets close.

--- Assets/Scripts/GoombaController.cs
+++ Assets/Scripts/GoombaController.cs
@@ -3,18 +3,40 @@ using System.Collections;
 
 public class GoombaController : MonoBehaviour {
 
+
     public int speed;
 
+    public Vector3 rotationSpeed;
+
+    public GameObject thirdPerson;
+
+    public float enemyLookDistance;
+
+    private float playerDistance;
+
     private Rigidbody rb;
+    private Rigidbody rbThirdPerson;
 
 	// Use this for initialization
 	void Start () {
         rb = GetComponent<Rigidbody>();
-
+        rbThirdPerson = thirdPerson.GetComponent<Rigidbody>();
     }
 	
 	// Update is called once per frame
 	void Update () {
-        rb.velocity = new Vector3(speed, rb.velocity.y, 0.0f);
-	}
+        playerDistance = Vector3.Distance(rbThirdPerson.transform.position, transform.position);
+
+        if (playerDistance <= enemyLookDistance)
+        {
+            transform.LookAt(rbThirdPerson.transform);
+        }
+        else
+        {
+            transform.Rotate(rotationSpeed * Time.deltaTime);
+        }
+
+        transform.position += transform.forward * Time.deltaTime * speed;
+
+    }
 }
